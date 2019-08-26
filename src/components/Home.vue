fb82533aa707684c58b843967f4fb87211186cf7
AlfaRojo <kortiz@correo.url.edu.gt>
2019-08-26 07:32:34 -0600

    Subido

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <h1>{{ Calculadora JS }}</h1>
+        <p>Estructura de Datos I</a>.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        props: {
+            msg: String
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
